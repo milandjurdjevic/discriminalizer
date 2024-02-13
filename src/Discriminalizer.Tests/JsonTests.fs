6796d47e51c7eadc8ba3cd044298aa2d7d83815c
Milan Djurdjevic <milan_djurdjevic@icloud.com>
2024-02-13 23:50:53 +0100

    Make tests more understandable

--- src/Discriminalizer.Tests/JsonTests.fs
+++ src/Discriminalizer.Tests/JsonTests.fs
@@ -4,53 +4,54 @@ module Discriminalizer.JsonTests
 open System.IO
 open System.Text
 open System.Text.Json
+open System.Text.Json.Serialization
 open System.Threading
 open VerifyXunit
 open Xunit
 
 [<AbstractClass>]
-type Shape() =
-    abstract member Sides: float
-    abstract member Dimensions: float
-    abstract member Name: string
+type Animal() =
+    [<JsonPropertyName("Type")>]
+    member val AnimalType: string = "" with get, set
 
-type Circle() =
-    inherit Shape()
-    member val Radius = 0.0 with get, set
-    override val Name = "Circle"
-    override val Sides = 1
-    override val Dimensions = 2
+    [<JsonPropertyName("Origin")>]
+    member val AnimalOrigin: string = "" with get, set
 
-type Rectangle() =
-    inherit Shape()
-    member val Height = 0.0 with get, set
-    member val Width = 0.0 with get, set
-    override val Name = "Rectangle"
-    override val Sides = 4
-    override val Dimensions = 2
+    member this.ClassType: string = this.GetType().Name
 
-type Tetrahedron() =
-    inherit Shape()
-    member val Volume = 0.0 with get, set
-    override val Name = "Tetrahedron"
-    override val Sides = 4
-    override val Dimensions = 3
+type Cat() =
+    inherit Animal()
+
+type DomesticCat() =
+    inherit Cat()
+
+type WildCat() =
+    inherit Cat()
+
+type Dog() =
+    inherit Animal()
+
+type DomesticDog() =
+    inherit Dog()
+
+type WildDog() =
+    inherit Dog()
 
 [<Theory>]
 // Deserialize single object that is defined in a scheme.
 // lang=json
-[<InlineData("""{"Name": "Circle", "Radius": 5.0 }""")>]
-// Deserialize array that contains objects without scheme.
+[<InlineData("""{"Type": "Dog", "Origin": "Domestic" }""")>]
+// Deserialize array that contains objects without scheme (schemaless).
 // lang=json
 [<InlineData("""
 [
   { 
-    "Info": { "Name": "Circle", "Radius": 5.0 }
+    "Animal": { "Type": "Fish", "Origin": "Wild" }
   },
   {
-    "Data": [
-      { "Name": "Name", "Value": "Circle" },
-      { "Name": "Radius", "Value": 5.0 }
+    "Animals": [
+      { "Type": "Fish", "Origin": "Wild" },
+      { "Type": "Fish", "Origin": "Domestic" }
     ]
   }
 ]""")>]
@@ -58,37 +59,38 @@ type Tetrahedron() =
 // lang=json
 [<InlineData("""
 [
-  { "Name": "Circle", "Radius": 5.0 },
-  { "Sides": 4, "Dimensions": 2, "Height": 10.0, "Width": 8.0 },
-  { "Sides": 4, "Dimensions": 3, "Volume": 100 }
+  {"Type": "Dog", "Origin": "Domestic" },
+  {"Type": "Cat", "Origin": "Domestic" },
+  {"Type": "Cat", "Origin": "Wild" },
+  {"Type": "Cat" },
+  {"Type": "Dog" }
 ]
 """)>]
 // Deserialize array that contains a null object.
 // lang=json
-[<InlineData("""[{"Name": "Circle"}, null]""")>]
+[<InlineData("""[{"Type": "Dog"}, null]""")>]
 let ``The deserialized input matches the verified output`` (json: string) =
     async {
+        let discriminators =
+            [|
+               // Type and Origin based discriminator
+               Discriminator("Type", "Origin")
+                   .With<WildDog>("Dog", "Wild")
+                   .With<DomesticDog>("Dog", "Domestic")
+                   .With<WildCat>("Cat", "Wild")
+                   .With<DomesticCat>("Cat", "Domestic")
+               // Type based discriminator
+               Discriminator("Type").With<Cat>("Cat").With<Dog>("Dog") |]
 
-        let discriminator1 =
-            Discriminator("Sides", "Dimensions")
-                .With<Circle>("1", "2")
-                .With<Rectangle>("4", "2")
-                .With<Tetrahedron>("4", "3")
-
-        let discriminator2 =
-            Discriminator("Name")
-                .With<Circle>("Circle")
-                .With<Rectangle>("Rectangle")
-                .With<Tetrahedron>("Tetrahedron")
-
-        let discriminators = [| discriminator1; discriminator2 |]
         let bytes = Encoding.UTF8.GetBytes json
-        let options = JsonSerializerOptions()
         use stream = new MemoryStream(bytes)
 
-        let! output =
-            Json.OfStream stream discriminators options CancellationToken.None
-            |> Async.AwaitTask
+        let options =
+            { Serializer = JsonSerializerOptions()
+              Discriminators = discriminators
+              IncludeSchemaless = true }
+
+        let! output = Json.OfStream stream options CancellationToken.None |> Async.AwaitTask
 
         do!
             Verifier.Verify(output).UseParameters(json).HashParameters().ToTask()
--- src/Discriminalizer/Json.fs
+++ src/Discriminalizer/Json.fs
@@ -1,61 +1,68 @@
-module Discriminalizer.Json
+namespace Discriminalizer
 
-open System
-open System.Collections.Generic
-open System.IO
 open System.Text.Json
-open System.Text.Json.Nodes
-open System.Threading
-open Microsoft.FSharp.Core
 
-let private tryFindNodeValue (node: JsonNode) (path: string) : string option =
-    node.AsObject()
-    |> Seq.tryFind (_.Key.Equals(path, StringComparison.OrdinalIgnoreCase))
-    |> fun value ->
-        match value with
-        | None -> None
-        | Some some -> Some(some.Value.ToString())
+type JsonOptions =
+    { Serializer: JsonSerializerOptions
+      Discriminators: Discriminator seq
+      IncludeSchemaless: bool }
 
-let rec private toSchemaless (node: JsonNode) =
-    match node with
-    | :? JsonValue as jsonValue -> jsonValue.ToString() :> obj
-    | :? JsonObject as jsonObject -> jsonObject |> Seq.map (fun x -> x.Key, toSchemaless x.Value) |> readOnlyDict :> obj
-    | :? JsonArray as jsonArray -> jsonArray |> Seq.map toSchemaless :> obj
-    | _ -> failwith "Unsupported Json Type"
+module Json =
 
-let private ofObject (discriminators: Discriminator seq) (options: JsonSerializerOptions) (json: JsonNode) =
-    discriminators
-    |> Seq.map (fun discriminator -> discriminator, discriminator.Paths |> Seq.map (tryFindNodeValue json))
-    |> Seq.filter (fun (_, values) -> values |> Seq.forall (fun value -> value.IsSome))
-    |> Seq.map (fun (discriminator, values) -> discriminator, values |> Seq.map (_.Value))
-    |> Seq.map (fun (discriminator, values) -> discriminator.Values.TryGetValue(values |> String.concat String.Empty))
-    |> Seq.filter fst
-    |> Seq.map snd
-    |> Seq.tryHead
-    |> fun returnType ->
-        match returnType with
-        | None -> json |> toSchemaless
-        | Some some -> json.Deserialize(some, options)
+    open System
+    open System.IO
+    open System.Text.Json.Nodes
+    open System.Threading
+    open Microsoft.FSharp.Core
 
-let private ofArray (discriminators: Discriminator seq) (options: JsonSerializerOptions) (json: JsonArray) =
-    json
-    |> Seq.filter (fun i -> not (isNull i))
-    |> Seq.map (ofObject discriminators options)
+    let private tryFindNodeValue (node: JsonNode) (path: string) : string option =
+        node.AsObject()
+        |> Seq.tryFind (_.Key.Equals(path, StringComparison.OrdinalIgnoreCase))
+        |> fun value ->
+            match value with
+            | None -> None
+            | Some some -> Some(some.Value.ToString())
 
-let private enumerate object = seq { yield object }
+    let rec private toSchemaless (node: JsonNode) =
+        match node with
+        | :? JsonValue as jsonValue -> jsonValue.ToString() :> obj
+        | :? JsonObject as jsonObject ->
+            jsonObject
+            |> Seq.map (fun node -> node.Key, toSchemaless node.Value)
+            |> readOnlyDict
+            :> obj
+        | :? JsonArray as jsonArray -> jsonArray |> Seq.map toSchemaless :> obj
+        | _ -> failwith "Unsupported Json Type"
 
-let OfStream
-    (stream: Stream)
-    (discriminators: IReadOnlyCollection<Discriminator>)
-    (options: JsonSerializerOptions)
-    (cancellationToken: CancellationToken)
-    =
-    task {
-        let! node = JsonSerializer.DeserializeAsync<JsonNode>(stream, options, cancellationToken)
+    let private ofObject (discriminators: Discriminator seq) (options: JsonSerializerOptions) (json: JsonNode) =
+        discriminators
+        |> Seq.map (fun discriminator -> discriminator, discriminator.Paths |> Seq.map (tryFindNodeValue json))
+        |> Seq.filter (fun (_, values) -> values |> Seq.forall (fun value -> value.IsSome))
+        |> Seq.map (fun (discriminator, values) -> discriminator, values |> Seq.map (_.Value))
+        |> Seq.map (fun (discriminator, values) ->
+            discriminator.Values.TryGetValue(values |> String.concat String.Empty))
+        |> Seq.filter fst
+        |> Seq.map snd
+        |> Seq.tryHead
+        |> fun returnType ->
+            match returnType with
+            | None -> json |> toSchemaless
+            | Some some -> json.Deserialize(some, options)
 
-        return
-            match node with
-            | :? JsonArray as array -> ofArray discriminators options array
-            | :? JsonObject as object -> ofObject discriminators options object |> enumerate
-            | _ -> Seq.empty<obj>
-    }
+    let private ofArray (discriminators: Discriminator seq) (options: JsonSerializerOptions) (json: JsonArray) =
+        json
+        |> Seq.filter (fun i -> not (isNull i))
+        |> Seq.map (ofObject discriminators options)
+
+    let private enumerate object = seq { yield object }
+
+    let OfStream (stream: Stream) (options: JsonOptions) (cancellationToken: CancellationToken) =
+        task {
+            let! node = JsonSerializer.DeserializeAsync<JsonNode>(stream, options.Serializer, cancellationToken)
+
+            return
+                match node with
+                | :? JsonArray as array -> ofArray options.Discriminators options.Serializer array
+                | :? JsonObject as object -> ofObject options.Discriminators options.Serializer object |> enumerate
+                | _ -> Seq.empty<obj>
+        }
